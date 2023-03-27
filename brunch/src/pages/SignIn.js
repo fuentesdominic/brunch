4aff4785b963bd63bbba22f462854c99df3890a2
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-27 11:05:58 -0500

    writing logic for signin

--- brunch/src/pages/SignIn.js
+++ brunch/src/pages/SignIn.js
@@ -1,5 +1,27 @@
-const SignIn = () => {
-  return (
+import { useState } from "react"
+import { SignInUser } from "../services/Auth"
+import { useNavigate } from 'react-router-dom'
+
+const SignIn = (props) => {
+
+    let navigate = useNavigate()
+
+    const [formValues, setFormValues] = useState({name: '', email: '' })
+
+    const handleChange = (e) => {
+        setFormValues({ ...formValues, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const payload = await SignInUser(formValues)
+        setFormValues({ name: '', email: '' })
+        props.setUser(payload)
+        props.toggleAuthenticated(true)
+        navigate('/home')
+    }
+
+    return (
     <div>SignIn</div>
   )
 }
