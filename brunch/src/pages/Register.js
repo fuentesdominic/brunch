3981689799d0b66aca3f9c8d0f76f5bcd700490f
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-27 11:16:17 -0500

    writing logic for register

--- brunch/src/pages/Register.js
+++ brunch/src/pages/Register.js
@@ -1,4 +1,37 @@
+import { useState } from 'react'
+import { RegisterUser } from '../services/Auth'
+import { useNavigate } from 'react-router-dom'
+
 const Register = () => {
+
+    let navigate = useNavigate()
+
+    const [formValues, setFormValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const handleChange = (e) => {
+        setFormValues({ ...formValues, [e.target.name]: e.target.value })
+      }
+
+      const handleSubmit = async (e) => {
+        e.preventDefault()
+        await RegisterUser({
+            name: formValues.name,
+            email: formValues.email,
+            password: formValues.password
+        })
+        setFormValues({
+            name: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        })
+        navigate('/')
+      }
   return (
     <div>Register</div>
   )
