4834fe4ff3606d1e2428e57a773de444462b7031
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-27 13:12:20 -0500

    fix routes

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ function App() {
           <Route path='/register' element={<Register />} />
           <Route path='/home' element={<Home />} />
           <Route path='/about' element={<About />} />
-          <Route path='/detail/:deatilId' element={<RestuarantDetails />} />
+          <Route path='/detail/:deatilId/user/:user' element={<RestuarantDetails />} />
           <Route path='/detail/detailId/user/:user/form' element={<RestaurantForms />} />
         </Routes>
       </main>
