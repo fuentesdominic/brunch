5e6b042e3c183ecedc7a50aff30e586c806399c4
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-27 14:35:39 -0500

    writing logic for navigating

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,30 @@
-const Navbar = () => {
+import { Link } from 'react-router-dom';
+
+const Navbar = ({user, handleLogOut}) => {
+  let userOptions;
+  if (user) {
+    userOptions = (
+      <nav className='nav-links'>
+        <Link className='nav-link' to='/home'>Home</Link>  
+        <Link className='nav-link' to='/about'>About</Link>
+        <Link className='nav-link' onClick={handleLogOut} to='/'>Sign Out</Link>    
+        </nav>
+    );
+  }
+
+  const publicOptions = (
+    <nav className='nav-links'>
+      <Link className='nav-link' to='/register'>Register</Link>
+      <Link className='nav-link' to='/'>Sign In</Link>
+      <Link className='nav-link' to='/about'>About</Link>
+    </nav>
+  );
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="iconDiv">
+      <h3 className="pageTitle">Brunch</h3>
+      </div>
+    </div>
   )
 }
 export default Navbar
