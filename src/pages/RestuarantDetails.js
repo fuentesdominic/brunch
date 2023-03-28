db574532b6d515991b79c7b9faac03f96839fbb6
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-28 17:49:48 -0500

    handle change for adding to menu

--- src/pages/RestuarantDetails.js
+++ src/pages/RestuarantDetails.js
@@ -1,6 +1,6 @@
-// import { useState, useEffect } from "react"
-// import axios from "axios"
-// import { useLocation, useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { useLocation, useNavigate } from "react-router-dom"
 
 const RestuarantDetails = () => {
   return (
