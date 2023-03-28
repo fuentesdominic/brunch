e8d65fd19d333831130da4282f2147ac71bd9122
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-28 17:56:25 -0500

    handle change added

--- src/pages/RestuarantDetails.js
+++ src/pages/RestuarantDetails.js
@@ -3,6 +3,37 @@ import axios from "axios"
 import { useLocation, useNavigate } from "react-router-dom"
 
 const RestuarantDetails = () => {
+  
+  const location = useLocation()
+  const navigate = useNavigate()
+  const restaurant = location.state
+  let restaurantText = ''
+
+  const [menus, setMenus] = useState()
+
+  const [updateRestaurant, setUpdateResaurant] = useState([])
+
+  const [newMenu, setNewMenu] = useState({
+    item: '',
+    price: '',
+    restaurant_id: restaurant._id 
+  })
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await axios.post(`/menu/create/${restaurant._id}`, newMenu)
+    getMenus()
+  }
+
+  const handleChange = (event) => {
+    setNewMenu({ ...newMenu, [event.target.name]: event.target.value})
+  }
+
+  const getMenus = async () => {
+    let res = await axios.get(`/menu/${restaurant._id}`)
+    setMenus(res.data)
+  }
+
   return (
     <div></div>
   )
