a1f97eddbc0d218c51bddbe0123fa6c17da4a581
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-28 19:41:19 -0500

    add handle change inside return

--- src/pages/RestuarantDetails.js
+++ src/pages/RestuarantDetails.js
@@ -56,7 +56,57 @@ const RestuarantDetails = () => {
   }, [])
 
   return (
-    <div></div>
-  )
-}
+    <div className="detailsBody">
+      <img className="detailsImage" src={`${restaurant.picture_url}`} />
+    <div className="nameAndLocation">
+        <h1>{restaurant.name}</h1>
+        <h2 className="restaurantMM">{restaurant.mile_marker}</h2>
+      </div>
+  
+        {menus && menus.length ? (
+          <div className="menuListDiv" style={{ border: '1px solid #1f728d' }}>
+            <h2 className="menuListText">{menuText}</h2>
+            {menus &&
+              menus.map((oneMenu) => (
+                <div key={oneMenu._id} className="card">
+                  <h3>{oneMenu.name}</h3>
+                  {oneMenu.price ? (
+                    <h3 className="price">
+                      {' '}
+                      - Price: ${oneMenu.price} 
+                    </h3>
+                  ) : (
+                    <></>
+                  )}
+                  <img
+                    id="deleteMenu"
+                    onClick={() => deleteMenu(oneMenu)}
+                    className="trashIcon"
+                    alt="trash icon"
+                    src="https://cdn-icons-png.flaticon.com/512/542/542724.png"
+                  />
+                </div>
+              ))}
+          </div>
+          ) : (
+            <h2 className="menuListText">{menuText}</h2>
+          )}
+          <h2 className="createMenuText">Create New Menu Item</h2>
+          <form onSubmit={(e) => handleSubmit(e, updateRestaurant.id)}>
+            <input
+              name="item"
+              value={newMenu.item}
+              type="text"
+              placeholder="Item name"
+              onChange={handleChange} />
+            <input 
+              name="price"
+              value={newMenu.price}
+              type="text"
+              placeholder="Price: $"
+              onChange={handleChange} />  
+          </form>
+          </div>
+  )}
+  
 export default RestuarantDetails
