e86d1feb665245f735d9a462ae260be85dd99fb4
Dominic Fuentes <militaryforce1@yahoo.com>
2023-03-28 19:51:19 -0500

    set state and effect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,27 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import { Link } from 'react-router-dom'
+
 const Home = () => {
+
+  const [allRestaurants, setAllRestaurants] = useState()
+
+  const getAllRestaurants = async () => {
+    try {
+      let res = await axios.get('/restuarant/')
+      setAllRestaurants(res.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    getAllRestaurants()
+  }, [])
+
+
   return (
-    <div>Home</div>
+    <div className="homeBody"></div>
   )
 }
 export default Home
