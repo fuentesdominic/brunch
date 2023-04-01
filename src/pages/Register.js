baad7751bb8a994e57809fd2a812463663caff64
Dominic Fuentes <militaryforce1@yahoo.com>
2023-04-01 13:39:47 -0500

    finish styling for register

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -33,47 +33,55 @@ const Register = () => {
         navigate('/')
       }
   return (
-    <div className='signIn main'>
+    <div className='registerMain'>
+      <h1 className='registerTitle'>Register</h1>
+      <img className="registerImage" src="https://www.worldatlas.com/upload/70/97/1d/shutterstock-1478766713.jpg" alt=""></img>
       <div className='signIn text'>
       <form className='signIn form' onSubmit={handleSubmit}>
         <div className='signIn'>
-          <label htmlFor='name'>Name</label>
+          <label className='registerLabel' htmlFor='name'>Name: </label>
           <input 
             onChange={handleChange}
             name='name'
             type='name'
             placeholder='John Smith'
+            className='registerInput'
             value={formValues.name}
             required />
           </div>
           <div className='signIn'>
-            <label htmlFor='email'>Email</label>
+            <label className='registerLabel' htmlFor='email'>Email: </label>
             <input 
               onChange={handleChange}
               name='email'
               type='email'
               placeholder='JohnSmith@email.com'
+              className='registerInput'
               value={formValues.email}
               required />
           </div>
           <div className='signIn'>
-            <label htmlFor='password'>Password</label>
+            <label className='registerLabel' htmlFor='password'>Password: </label>
             <input
               onChange={handleChange}
               type='password'
               name='password'
+              placeholder='1234'
+              className='registerInput'
               value={formValues.password}
               required />
           <div className='signIn'>
-            <label htmlFor='confirmPassword'>Confirm Password</label>
+            <label className='registerLabel' htmlFor='confirmPassword'>Confirm Password: </label>
             <input 
               onChange={handleChange}
               type='password'
               name='confirmPassword'
+              placeholder='1234'
+              className='registerInput'
               value={formValues.confirmPassword}
               required />
           </div>
-          <button 
+          <button className='registerButton'
             disabled={!formValues.email || 
               (!formValues.password && 
                 formValues.confirmPassword === formValues.password)}>
