6d5a7a9a9eeaff6118946e56095e9f19eba4a726
Dominic Fuentes <militaryforce1@yahoo.com>
2023-04-04 23:08:18 -0500

    axios error

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'https://brunch-backend.herokuapp.com/'
+export const BASE_URL = 'https://git.heroku.com/brunch-backend.git'
 const Client = Axios.create({ baseURL: BASE_URL })
 
 Client.interceptors.request.use(
