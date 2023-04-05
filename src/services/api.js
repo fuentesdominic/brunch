e196bc24224046fdc3f1cabad3891a7d28556e34
Dominic Fuentes <militaryforce1@yahoo.com>
2023-04-04 23:32:11 -0500

    fixing cors

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'https://brunch-backend.herokuapp.com/'
+export const BASE_URL = 'https://brunch-backend.herokuapp.com'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
